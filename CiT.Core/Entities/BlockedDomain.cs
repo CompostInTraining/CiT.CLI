f8e1d5b1d6e89af672dcb8f4c88d45e9e5ce01b6
prplecake <me@prplecake.com>
2023-02-05 21:06:53 -0600

    Update BlockedDomain JsonProperties

--- CiT.Core/Entities/BlockedDomain.cs
+++ CiT.Core/Entities/BlockedDomain.cs
@@ -5,20 +5,15 @@ namespace CiT.Core.Entities;
 
 public class BlockedDomain
 {
-    public string? Id { get; set; }
-    public string? Domain { get; set; }
-    [JsonProperty("created_at")]
-    public DateTime CreatedAt { get; set; }
-    public string? Severity { get; set; }
-    [JsonProperty("reject_media")]
-    public bool RejectMedia { get; set; }
-    [JsonProperty("reject_reports")]
-    public bool RejectReports { get; set; }
-    [JsonProperty("private_comment")]
-    public string? PrivateComment { get; set; }
-    [JsonProperty("public_comment")]
-    public string? PublicComment { get; set; }
-    public bool Obfuscate { get; set; }
+    [JsonProperty("id")] public string? Id { get; set; }
+    [JsonProperty("domain")] public string? Domain { get; set; }
+    [JsonProperty("created_at")] public DateTime CreatedAt { get; set; }
+    [JsonProperty("severity")] public string? Severity { get; set; }
+    [JsonProperty("reject_media")] public bool RejectMedia { get; set; }
+    [JsonProperty("reject_reports")] public bool RejectReports { get; set; }
+    [JsonProperty("private_comment")] public string? PrivateComment { get; set; }
+    [JsonProperty("public_comment")] public string? PublicComment { get; set; }
+    [JsonProperty("obfuscate")] public bool Obfuscate { get; set; }
     /// <inheritdoc />
     public override string ToString() => $"{Domain}";
 }
