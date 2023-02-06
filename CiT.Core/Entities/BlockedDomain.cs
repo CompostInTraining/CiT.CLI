0d027dfcc971a56635d1c5d3d621cbe0732dd35d
prplecake <me@prplecake.com>
2023-02-05 20:06:44 -0600

    Bind JsonProperties

--- CiT.Core/Entities/BlockedDomain.cs
+++ CiT.Core/Entities/BlockedDomain.cs
@@ -1,4 +1,5 @@
 using System;
+using Newtonsoft.Json;
 
 namespace CiT.Core.Entities;
 
@@ -6,11 +7,16 @@ public class BlockedDomain
 {
     public string? Id { get; set; }
     public string? Domain { get; set; }
+    [JsonProperty("created_at")]
     public DateTime CreatedAt { get; set; }
     public string? Severity { get; set; }
+    [JsonProperty("reject_media")]
     public bool RejectMedia { get; set; }
+    [JsonProperty("reject_reports")]
     public bool RejectReports { get; set; }
+    [JsonProperty("private_comment")]
     public string? PrivateComment { get; set; }
+    [JsonProperty("public_comment")]
     public string? PublicComment { get; set; }
     public bool Obfuscate { get; set; }
     /// <inheritdoc />
