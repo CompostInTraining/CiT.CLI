9103293f977af77b551dd6939c691e0a05d276b2
prplecake <me@prplecake.com>
2023-02-07 00:36:55 -0600

    Remove unused using

--- CiT.Core/Mastodon/DomainBlocksApi.cs
+++ CiT.Core/Mastodon/DomainBlocksApi.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using CiT.Core.Configuration;
