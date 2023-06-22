fe63a24d141861d6811e45e73884123724905bd3
prplecake <me@prplecake.com>
2023-06-21 20:23:45 -0500

    Add sample app

---
+++ src/ConsoleAppTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.CommandLine;
+using ConsoleAppTest.Commands;
+class Program
+{
+    async static Task Main(string[] args)
+    {
+        var rootCommand = new RootCommand("Sample command-line app");
+
+        rootCommand.SetHandler(() => { Console.WriteLine("Hello, World!"); });
+        var subCommand1 = SubCommand1.BuildCommand();
+        rootCommand.Add(subCommand1);
+        await rootCommand.InvokeAsync(args);
+    }
+}
