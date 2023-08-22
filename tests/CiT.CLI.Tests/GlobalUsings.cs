8c596356bdee4d9b78c3c1eecc25e05a74d17c93
prplecake <me@prplecake.com>
2023-08-22 14:26:11 -0500

    Remove CLI tests

--- tests/CiT.CLI.Tests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-// Global using directives
-
-global using CiT.CLI.Commands;
-global using CiT.CLI.Tests.Helpers;
-global using CiT.Core.Configuration;
-global using CiT.Core.Entities;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using Moq;
