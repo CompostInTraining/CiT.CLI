fff12badfba25598ee5e880a96328489eaf4d3b9
Matty Jorgensen <me@prplecake.com>
2025-03-21 10:58:27 -0500

    Fix sonarqube issues

--- src/CiT.Core/Mastodon/InstanceApi.cs
+++ src/CiT.Core/Mastodon/InstanceApi.cs
@@ -4,19 +4,18 @@ namespace CiT.Core.Mastodon;
 
 public class InstanceApi : ApiClient
 {
-    private static string? _instancesApiUrl;
+    private readonly string? _instancesApiUrl;
     public InstanceApi(IConfigManager configManager, HttpClient client) : base(configManager, client)
     {
         _instancesApiUrl = $"{configManager.Instance.Url}/api/v1/instance";
     }
     public async Task<List<string>> GetInstancePeers()
     {
-        List<string> results = [];
         var response = await Client.GetAsync($"{_instancesApiUrl}/peers");
         if (response.StatusCode is HttpStatusCode.NotFound)
             throw new InstanceDiscoveryDisabledException();
         var responseContent = await response.Content.ReadAsStringAsync();
-        results = JsonConvert.DeserializeObject<List<string>>(responseContent) ?? [];
+        var results = JsonConvert.DeserializeObject<List<string>>(responseContent) ?? [];
         return results;
     }
 }
